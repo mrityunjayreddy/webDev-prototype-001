2cfe70647df779717eef2a80afe9f4ab88089a0d
Mrityunjay <mrityunjayreddy04@gmail.com>
2024-12-03 19:44:48 +0530

    Initial commit to the repository

---
+++ coreserviceone/src/main/java/com/project/prototype/coreserviceone/CoreserviceoneApplication.java
@@ -0,0 +1,13 @@
+package com.project.prototype.coreserviceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreserviceoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreserviceoneApplication.class, args);
+	}
+
+}
---
+++ coreserviceone/src/test/java/com/project/prototype/coreserviceone/CoreserviceoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prototype.coreserviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreserviceoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
